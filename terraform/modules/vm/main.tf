83f90e3bce5d011c9e77587cba112613a6428c60
Pochy9024 <pochy9024@gmail.com>
2025-09-02 22:45:23 +0200

    add rule to nic nsg to allow http traffic to port 80

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -9,16 +9,17 @@ resource "azurerm_network_security_group" "vm_nsg" {
   resource_group_name = azurerm_resource_group.vm_rg.name
 
   security_rule {
-    name                       = "AllowHTTPFromLB"
-    priority                   = 100
+    name                       = "AllowHTTPInbound"
+    priority                   = 120
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "80"
-    source_address_prefix      = "AzureLoadBalancer"
+    source_address_prefix      = "Internet"
     destination_address_prefix = "*"
   }
+
 
   security_rule {
     name                       = "AllowSSHFromBastion"
